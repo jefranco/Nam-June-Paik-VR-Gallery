49cb182fdeacb4d6f76b4c0ee6a53082a21ef9b7
Jesus Franco <55300629+jefranco@users.noreply.github.com>
2024-12-01 22:49:06 -0700

    Text + Timer progress

--- Assets/TimerCountdown.cs
+++ Assets/TimerCountdown.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimerCountdown : MonoBehaviour
 {
+    public float timeInSec;
+    public Text clockText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeInSec -= Time.deltaTime;
+        clockText.text = timeInSec.ToString();
+
+        if (timeInSec <= 0.0f)
+        {
+            timerEnded();
+        }
+
+    }
+
+    void timerEnded()
+    {
+        //do your stuff here.
     }
 }
